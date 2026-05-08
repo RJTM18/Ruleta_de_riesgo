6723330a60c818416e8e76d7951797a7dd2b1ff4
Rafael Tito <rafael.titom2007@gmail.com>
2026-05-08 18:21:23 -0500

    cuerpo principal y scrip para generar los numeros

--- index.cpp
+++ index.cpp
@@ -9,22 +9,22 @@ int main() {
     char respuesta;
 	int cart_3, sum_total;
 
-	cout << "Bienvenido a BlackJack";
-	cout << "¿Quieres comenzar (y/n)?";
+	cout << "Bienvenido a BlackJack\n";
+	cout << "¿Quieres comenzar (y/n)?\n  ";
 	cin >> ans; if (ans == 'n') { return 0; }
 
-	cout << "Comienza el juego!!!";
+	cout << "Comienza el juego!!!\n";
 
-	cout << "Recibes la primera carta: " << cart_1;
-	cout << "Recibes la segunda carta: " << cart_2;
+	cout << "Recibes la primera carta:\n " << cart_1 << endl;
+	cout << "Recibes la segunda carta:\n " << cart_2 << endl;
 
 	//funciones que generen los valores de carta 1, 2 y 3
     // sum_1y2 va a ser mayor que 0 de todas maneras
 
-	if (sum_1y2 < 21) { cout << "¿Desea sacar una tercera carta (y/n)?"; cin >> respuesta; }
+	if (sum_1y2 < 21) { cout << "¿Desea sacar una tercera carta (y/n)?\n"; cin >> respuesta ; }
 
 	if (respuesta == 'y') {
-		cout << "Recibes la tercera carta: " << cart_3;
+		cout << "Recibes la tercera carta:\n " << cart_3 << endl;
 		sum_total = cart_3 + sum_1y2;
 
 		if (sum_total > 21) { cout << "Te pasaste. Perdiste"; return 0; }
