19d5f16b31b715150db67e38ea5743b683bd6935
Alejo... <ale986413ap@gmail.com>
2026-05-13 17:53:31 -0500

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -1,44 +1,86 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main() {
 
+    srand(time(0));
+
 	char ans;
-	int cart_1, cart_2, sum_1y2 = cart_1 + cart_2;
+	int cart_1, cart_2, sum_1y2;
     char respuesta;
 	int cart_3, sum_total;
 
 	cout << "Bienvenido a BlackJack\n";
 	cout << "¿Quieres comenzar (y/n)?\n  ";
-	cin >> ans; if (ans == 'n') { return 0; }
+	cin >> ans;
+
+    if (ans == 'n') {
+        return 0;
+    }
 
 	cout << "Comienza el juego!!!\n";
 
-	//funciones que generen los valores de carta 1, 2 y 3
+	// numeros aleatorios
+    cart_1 = rand() % 10 + 1;
+    cart_2 = rand() % 10 + 1;
+    cart_3 = rand() % 10 + 1;
+
+    // suma correcta
+    sum_1y2 = cart_1 + cart_2;
 
 	cout << "Recibes la primera carta:\n " << cart_1 << endl;
 	cout << "Recibes la segunda carta:\n " << cart_2 << endl;
-	
-    // sum_1y2 va a ser mayor que 0 de todas maneras (por eso no pongo sum_1y2 > 0, seria redundante)
 
-	if (sum_1y2 < 21) { cout << "¿Desea sacar una tercera carta (y/n)?\n"; cin >> respuesta ; }
+	if (sum_1y2 < 21) {
+        cout << "¿Desea sacar una tercera carta (y/n)?\n";
+        cin >> respuesta;
+    }
 
 	if (respuesta == 'y') {
+
 		cout << "Recibes la tercera carta:\n " << cart_3 << endl;
+
 		sum_total = cart_3 + sum_1y2;
 
-		if (sum_total > 21) { cout << "Te pasaste. Perdiste"; return 0; }
-		else if (sum_total == 21) { cout << "Ganaste con 21"; return 0; }
+		if (sum_total > 21) {
+            cout << "Te pasaste. Perdiste";
+            return 0;
+        }
+
+		else if (sum_total == 21) {
+            cout << "Ganaste con 21";
+            return 0;
+        }
+
 		else {
-            if (sum_total >= 17) { cout << "Ganaste"; return 0; }
-		    else { cout << "Perdiste"; return 0; }
+
+            if (sum_total >= 17) {
+                cout << "Ganaste";
+                return 0;
+            }
+
+		    else {
+                cout << "Perdiste";
+                return 0;
+            }
         }
 	}
+
 	else if (respuesta == 'n') {
-		if (sum_1y2 >= 17) { cout << "Ganaste"; return 0; }
-		else { cout << "Perdiste"; return 0; }
+
+		if (sum_1y2 >= 17) {
+            cout << "Ganaste";
+            return 0;
+        }
+
+		else {
+            cout << "Perdiste";
+            return 0;
+        }
 	}
 
-	return 0; //HICE NUEVOS CAMBIOS
+	return 0;
 }
