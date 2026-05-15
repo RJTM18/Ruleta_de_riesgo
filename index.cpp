68e52b266fa19e10a203f0592da3704bb06e011e
Alejo... <ale986413ap@gmail.com>
2026-05-14 20:59:05 -0500

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -17,103 +17,75 @@ int main() {
 
     while (jugar == 'y') {
 
-        int opcion;
+        char ans;
+        int cart_1, cart_2, sum_1y2;
+        char respuesta = 'n';
+        int cart_3, sum_total;
 
         cout << "\n=====================\n";
         cout << "      BLACKJACK\n";
         cout << "=====================\n";
 
-        cout << "1. Jugar\n";
-        cout << "2. Ver reglas\n";
-        cout << "3. Salir\n";
-        cout << "Seleccione una opcion: ";
-        cin >> opcion;
+        cout << "¿Quieres comenzar (y/n)?\n";
+        cin >> ans;
 
-        if (opcion == 3) {
+        if (ans == 'n') {
             return 0;
         }
 
-        if (opcion == 2) {
+        cout << "\nComienza el juego!!!\n";
 
-            cout << "\n===== REGLAS =====\n";
-            cout << "- Debes acercarte a 21.\n";
-            cout << "- Si pasas de 21, pierdes.\n";
-            cout << "- Si obtienes entre 17 y 21, ganas.\n";
+        cart_1 = generarCarta();
+        cart_2 = generarCarta();
+        cart_3 = generarCarta();
+
+        sum_1y2 = cart_1 + cart_2;
+
+        // Primera carta
+        cout << "\nPrimera carta:\n";
+        cout << "+-----+\n";
+        cout << "|  " << cart_1 << "  |\n";
+        cout << "+-----+\n";
+
+        // Segunda carta
+        cout << "\nSegunda carta:\n";
+        cout << "+-----+\n";
+        cout << "|  " << cart_2 << "  |\n";
+        cout << "+-----+\n";
+
+        cout << "\nSuma actual: " << sum_1y2 << endl;
+
+        if (sum_1y2 < 21) {
+
+            cout << "\n¿Desea sacar una tercera carta? (y/n)\n";
+            cin >> respuesta;
         }
 
-        if (opcion == 1) {
+        if (respuesta == 'y') {
 
-            int cart_1, cart_2, sum_1y2;
-            char respuesta = 'n';
-            int cart_3, sum_total;
-
-            cout << "\nComienza el juego!!!\n";
-
-            cart_1 = generarCarta();
-            cart_2 = generarCarta();
-            cart_3 = generarCarta();
-
-            sum_1y2 = cart_1 + cart_2;
-
-            // Primera carta
-            cout << "\nPrimera carta:\n";
+            // Tercera carta
+            cout << "\nTercera carta:\n";
             cout << "+-----+\n";
-            cout << "|  " << cart_1 << "  |\n";
+            cout << "|  " << cart_3 << "  |\n";
             cout << "+-----+\n";
 
-            // Segunda carta
-            cout << "\nSegunda carta:\n";
-            cout << "+-----+\n";
-            cout << "|  " << cart_2 << "  |\n";
-            cout << "+-----+\n";
+            sum_total = cart_3 + sum_1y2;
 
-            cout << "\nSuma actual: " << sum_1y2 << endl;
+            cout << "\nSuma total: " << sum_total << endl;
 
-            if (sum_1y2 < 21) {
+            if (sum_total > 21) {
 
-                cout << "\nDesea sacar una tercera carta? (y/n)\n";
-                cin >> respuesta;
+                cout << "\nTe pasaste. Perdiste\n";
             }
 
-            if (respuesta == 'y') {
+            else if (sum_total == 21) {
 
-                // Tercera carta
-                cout << "\nTercera carta:\n";
-                cout << "+-----+\n";
-                cout << "|  " << cart_3 << "  |\n";
-                cout << "+-----+\n";
-
-                sum_total = cart_3 + sum_1y2;
-
-                cout << "\nSuma total: " << sum_total << endl;
-
-                if (sum_total > 21) {
-
-                    cout << "\nTe pasaste. Perdiste\n";
-                }
-
-                else if (sum_total == 21) {
-
-                    cout << "\nGanaste con 21\n";
-                }
-
-                else {
-
-                    if (sum_total >= 17) {
-
-                        cout << "\nGanaste\n";
-                    }
-
-                    else {
-
-                        cout << "\nPerdiste\n";
-                    }
-                }
+                cout << "\nGanaste con 21\n";
             }
 
-            else if (respuesta == 'n') {
+            else {
 
-                if (sum_1y2 >= 17) {
+                if (sum_total >= 17) {
 
                     cout << "\nGanaste\n";
                 }
@@ -125,7 +97,20 @@ int main() {
             }
         }
 
-        cout << "\nQuieres jugar otra vez? (y/n): ";
+        else if (respuesta == 'n') {
+
+            if (sum_1y2 >= 17) {
+
+                cout << "\nGanaste\n";
+            }
+
+            else {
+
+                cout << "\nPerdiste\n";
+            }
+        }
+
+        cout << "\n¿Quieres jugar otra vez? (y/n): ";
         cin >> jugar;
     }
 
